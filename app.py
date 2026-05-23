1505d75b7b6cfa09ef919a3ca1bec231509601c6
muneebmanzoor230 <muneebmanzoor230@gmail.com>
2026-05-23 16:42:10 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -1,73 +1,10 @@
-from flask import Flask, render_template, request, redirect
-import json
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-TASK_FILE = 'tasks.json'
-
-
-def load_tasks():
-    if not os.path.exists(TASK_FILE):
-        return []
-
-    with open(TASK_FILE, 'r') as file:
-        return json.load(file)
-
-
-def save_tasks(tasks):
-    with open(TASK_FILE, 'w') as file:
-        json.dump(tasks, file, indent=4)
-
-
-@app.route('/')
+@app.route("/")
 def home():
-    tasks = load_tasks()
-    return render_template('index.html', tasks=tasks)
+    return "Task Student Project Running Successfully!"
 
-
-@app.route('/add', methods=['POST'])
-def add_task():
-    task = request.form.get('task')
-
-    tasks = load_tasks()
-
-    tasks.append({
-        'task': task,
-        'completed': False
-    })
-
-    save_tasks(tasks)
-
-    return redirect('/')
-
-
-@app.route('/complete/<int:index>')
-def complete_task(index):
-    tasks = load_tasks()
-
-    tasks[index]['completed'] = True
-
-    save_tasks(tasks)
-
-    return redirect('/')
-
-
-@app.route('/delete/<int:index>')
-def delete_task(index):
-    tasks = load_tasks()
-
-    tasks.pop(index)
-
-    save_tasks(tasks)
-
-    return redirect('/')
-
-
-@app.route('/api/tasks')
-def api_tasks():
-    return load_tasks()
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
